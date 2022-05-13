49a211448817aa3ca82efa1a87edb88d7452763e
Ali Elbashir <alielbashiir@gmail.com>
2022-05-13 16:00:35 +0300

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,6 @@
+def calculate_triangle_area(base, height):
+    return base * height / 2
+
+
+def calculate_rectangle_area(height, width):
+    return height * width
---
+++ test_calculator.py
@@ -0,0 +1,11 @@
+from calculator import *
+
+
+def test_calculate_rectangle_area():
+    """tests that a rectangle area is equal to height * width"""
+    assert calculate_rectangle_area(2, 3) == 6
+
+
+def test_calculate_triangle_area():
+    """tests that a triangle's area is equal to base * height / 2"""
+    assert calculate_triangle_area(2, 3) == 3
